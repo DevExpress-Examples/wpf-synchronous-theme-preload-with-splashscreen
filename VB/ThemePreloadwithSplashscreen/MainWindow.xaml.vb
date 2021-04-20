e31859262fa7524fb5106c8830a005449716c51a
DevExpressExampleBot <fakeEmail>
2021-04-20 09:23:42 +0300

    Source auto update [skip ci]

---
+++ VB/ThemePreloadwithSplashscreen/MainWindow.xaml.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+Imports System.Diagnostics
+Imports System.Windows
+
+Namespace ThemePreloadwithSplashscreen
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+
+		Friend Shared stopwatch As New Stopwatch()
+		Public Sub New()
+			InitializeComponent()
+			AddHandler Me.ContentRendered, AddressOf MainWindow_ContentRendered
+		End Sub
+
+		Private Sub MainWindow_ContentRendered(ByVal sender As Object, ByVal e As EventArgs)
+			stopwatch.Stop()
+			stopwatch.Stop()
+		End Sub
+
+		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			stopwatch.Start()
+			Dim richWindow As New RichWindow()
+			'App.Current.MainWindow = richWindow;
+			richWindow.Show()
+			'Close();
+		End Sub
+	End Class
+End Namespace
---
+++ VB/ThemePreloadwithSplashscreen/RichWindow.xaml.vb
@@ -0,0 +1,17 @@
+﻿Imports DevExpress.Xpf.Core
+Imports System
+Imports System.Windows
+
+Namespace ThemePreloadwithSplashscreen
+	''' <summary>
+	''' Interaction logic for RichWindow.xaml
+	''' </summary>
+	Partial Public Class RichWindow
+		Inherits ThemedWindow
+
+		Public Sub New()
+			InitializeComponent()
+			DataContext = New EmployeeViewModel()
+		End Sub
+	End Class
+End Namespace
