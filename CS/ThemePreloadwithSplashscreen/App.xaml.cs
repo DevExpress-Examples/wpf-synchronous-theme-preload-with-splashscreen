5f35d8c6dc387ed574958e96b97d141d2524c180
AndreySlabov <andrey.slabov@devexpress.com>
2022-12-07 17:28:00 +0400

    Update Sample

--- CS/ThemePreloadwithSplashscreen/App.xaml.cs
+++ CS/ThemePreloadwithSplashscreen/App.xaml.cs
@@ -2,20 +2,13 @@
 using DevExpress.Xpf.Grid;
 using DevExpress.Xpf.LayoutControl;
 using System;
-using System.Diagnostics;
 using System.Windows;
 
-namespace ThemePreloadwithSplashscreen
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
+namespace ThemePreloadwithSplashscreen {
+    public partial class App : Application {
 
         static Type[] types;
-        static App()
-        {
+        static App() {
             types = new Type[] { typeof(GridControl), typeof(LayoutControl) };
             SplashScreenManager.CreateThemed().ShowOnStartup();
             ThemeManager.PreloadThemeResourceAsync("Office2019Colorful", runTypeInitializers: false).Wait();
